9fe133502338aaf6e01db1d6db1cd8f82aaf625b
soban khan <ksoban20@gmail.com>
2016-09-30 22:52:30 +0530

    created a basic structural wire-rame of the website

---
+++ header.php
@@ -0,0 +1,12 @@
+<header>
+	<section class="logo">The Logo</section>
+	<nav class="menu">
+		<ul>
+			<li>Home</li>
+			<li>About</li>
+			<li>Acedemic</li>
+			<li>Portfolio</li>
+			<li>Contact</li>
+		</ul>
+	</nav>
+</header>
--- index.php
+++ index.php
@@ -2,13 +2,41 @@
 <head>
 	<title>st johns academy</title>
 	<link rel="stylesheet" type="text/css" href="bower_components/bootstrap/dist/css/bootstrap.min.css" />
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="bower_components/font-awesome/css/font-awesome.min.css" />
 	<link rel="stylesheet" href="dist/css/style.css" />
 </head>
 <body>
 	<script src="bower_components/jquery/dist/jquery.min.js"></script>
-	<h1 class="content fira-sans-2">hello world</h1>
-	<script src="https://use.typekit.net/pnv5mon.js"></script>
-	<script>try{Typekit.load({ async: true });}catch(e){}</script>
+	<!-- header starts -->
+	<?php include_once('header.php') ?>
+	<!-- header ends -->
+	<!-- slider starts -->
+	<?php include_once('slider.php') ?>
+	<!-- slider ends -->
+
+	<!-- prelude starts -->
+	<section class="prelude">
+		prelude
+	</section>
+	<!-- prelude ends -->
+
+	<!-- quick-ref starts -->
+	<section class="quick-ref">
+		quick-ref
+	</section>
+	<!-- quick-ref ends -->
+
+	<!-- events starts -->
+	<section class="events">
+		events
+	</section>
+	<!-- events ends -->
+
+	<!-- query-bar starts -->
+	<section class="query-bar">
+		query-bar
+	</section>
+	<!-- query-bar ends -->
 </body>
 </html>
