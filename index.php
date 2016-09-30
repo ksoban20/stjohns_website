ef1b81f5a6ce49fc0b9add2b6a4b7a5b84b82d05
soban khan <ksoban20@gmail.com>
2016-09-30 21:33:48 +0530

    initial framework done

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>st johns academy</title>
+	<link rel="stylesheet" type="text/css" href="bower_components/bootstrap/dist/css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="bower_components/font-awesome/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="dist/css/style.css" />
+</head>
+<body>
+	<script src="bower_components/jquery/dist/jquery.min.js"></script>
+	<div class="content">hello world</div>
+</body>
+</html>
