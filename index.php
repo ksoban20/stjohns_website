c6c5805ee1b5ec0c71ec74f6fd2876cb2e1b8137
soban khan <ksoban20@gmail.com>
2016-10-01 23:19:37 +0530

    added till notification stack

--- index.php
+++ index.php
@@ -69,7 +69,77 @@
 
 	<!-- events starts -->
 	<section class="events">
-		events
+		<div class="heading">Upcoming Events</div>
+		<div class="sub-heading">our upcoming events you should mind often</div>
+		<div class="notification-stack">
+			<div class="stack">
+				<div class="date">
+					<span class="date-wrapper">25</span> 
+					<span class="date-wrapper">jan</span> 
+					<span class="date-wrapper">2016</span>
+				</div>
+				<div class="content">
+					<div class="content-heading">Sports Day something somethings something</div>
+					<div class="content-text">lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </div>
+				</div>
+			</div>
+			<div class="stack">
+				<div class="date">
+					<span class="date-wrapper">25</span> 
+					<span class="date-wrapper">jan</span> 
+					<span class="date-wrapper">2016</span>
+				</div>
+				<div class="content">
+					<div class="content-heading">Sports Day something somethings something</div>
+					<div class="content-text">lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </div>
+				</div>
+			</div>
+			<div class="stack">
+				<div class="date">
+					<span class="date-wrapper">25</span> 
+					<span class="date-wrapper">jan</span> 
+					<span class="date-wrapper">2016</span>
+				</div>
+				<div class="content">
+					<div class="content-heading">Sports Day something somethings something</div>
+					<div class="content-text">lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </div>
+				</div>
+			</div>
+			<div class="stack">
+				<div class="date">
+					<span class="date-wrapper">25</span> 
+					<span class="date-wrapper">jan</span> 
+					<span class="date-wrapper">2016</span>
+				</div>
+				<div class="content">
+					<div class="content-heading">Sports Day something somethings something</div>
+					<div class="content-text">lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </div>
+				</div>
+			</div>
+			<div class="stack">
+				<div class="date">
+					<span class="date-wrapper">25</span> 
+					<span class="date-wrapper">jan</span> 
+					<span class="date-wrapper">2016</span>
+				</div>
+				<div class="content">
+					<div class="content-heading">Sports Day something somethings something</div>
+					<div class="content-text">lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </div>
+				</div>
+			</div>
+			<div class="stack">
+				<div class="date">
+					<span class="date-wrapper">25</span> 
+					<span class="date-wrapper">jan</span> 
+					<span class="date-wrapper">2016</span>
+				</div>
+				<div class="content">
+					<div class="content-heading">Sports Day something somethings something</div>
+					<div class="content-text">lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </div>
+				</div>
+			</div>
+			
+		</div>
 	</section>
 	<!-- events ends -->
 
