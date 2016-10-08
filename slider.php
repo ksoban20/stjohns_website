76fe8200f30ec22514fcde76bd26baf2dfadcb51
soban khan <ksoban20@gmail.com>
2016-10-08 14:54:47 +0530

    index reponsive on 480px width till quick-ref

--- slider.php
+++ slider.php
@@ -1,6 +1,6 @@
 <div class="slider">
 		<div class="slider-content">Lets Educate the world</div>
-		<div class="slider-heading">Saint Johns <strong>Academy</strong></div>
+		<div class="slider-heading">Saint Johns <strong>School</strong></div>
 		<div class="button-1">View Courses </div>
 		<div class="button-2">Contact us </div>
 	</div>
